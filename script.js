84ae2484a377d8341db478176e304cc1cf463728
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2020-10-10 18:49:33 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("keypress", function(event) {
+  if(event.code == "KeyA"){
+      let audioA = new Audio("media/A.mp3");
+      audioA.play();
+  } else if (event.code == "KeyS"){
+      let audioS = new Audio("media/S.mp3");
+      audioS.play();
+  } else if (event.code == "KeyD"){
+      let audioD = new Audio("media/D.mp3");
+      audioD.play();
+  } else if (event.code == "KeyF"){
+      let audioF = new Audio("media/F.mp3");
+      audioF.play();
+  } else if (event.code == "KeyG"){
+      let audioG = new Audio("media/G.mp3");
+      audioG.play();
+  } else if (event.code == "KeyH"){
+      let audioH = new Audio("media/H.mp3");
+      audioH.play();
+  } else if (event.code == "KeyJ"){
+      let audioJ = new Audio("media/J.mp3");
+      audioJ.play();
+  } else if (event.code == "KeyW"){
+      let audioW = new Audio("media/W.mp3");
+      audioW.play();
+  } else if (event.code == "KeyE"){
+      let audioE = new Audio("media/E.mp3");
+      audioE.play();
+  } else if (event.code == "KeyT"){
+      let audioT = new Audio("media/T.mp3");
+      audioT.play();
+  } else if (event.code == "KeyY"){
+      let audioY = new Audio("media/Y.mp3");
+      audioY.play();
+  } else if (event.code == "KeyU"){
+      let audioU = new Audio("media/U.mp3");
+      audioU.play();
+  }
+});
